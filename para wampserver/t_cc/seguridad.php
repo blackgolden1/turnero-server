ff3b42507a25c0a54179bee00024210afebcde51
Mario Orozco <mariodanielorozco@hotmail.com>
2024-08-15 17:37:59 -0500

    eliminando archivos innecesarios

--- para wampserver/t_cc/seguridad.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-if ($_SESSION['AUTENTICADO'] != "SI"){
-header("Location:index.php");
-exit();
-}
-?>
--- t_lb/seguridad.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-if ($_SESSION['AUTENTICADO'] != "SI"){
-header("Location:index.php");
-exit();
-}
